52e89ae8907f3bce431992de2e2579c20bbff520
Daniel Bergey <bergey@simspace.com>
2022-02-06 10:40:51 -0500

    paging forward by half, line, some debug info

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 use crossterm::{
     cursor,
-    event::{read, Event, KeyCode, KeyCode::Char},
+    event::{read, Event, KeyCode::Char, KeyModifiers},
     style::{self, Color, Stylize},
     terminal, ExecutableCommand, QueueableCommand,
 };
 use std::cmp::min;
-use std::fs::{self, File};
-use std::io::{self, Read, Result, Stdout, Write};
+use std::fs;
+use std::io::{self, Result, Stdout, Write};
 use unicode_segmentation::UnicodeSegmentation;
 
 // only unix / darwin for now
@@ -30,12 +30,17 @@ enum Action {
     Down,
     Left,
     Right,
-    PageDown,
+    PageDownFull,
+    PageDownLine,
+    PageDownHalf,
 }
 
 struct DisplayInfo {
     next_char: usize,
+    // these should always have the same length, outside display()
+    // TODO maybe should be Vec<LinePair> ?
     line_lengths: Vec<u16>,
+    line_starts: Vec<usize>,
 }
 
 fn await_input() -> Result<Action> {
@@ -43,12 +48,24 @@ fn await_input() -> Result<Action> {
         use Action::*;
         match read()? {
             Event::Key(event) => match event.code {
-                Char('q') => return Ok(Quit),
-                Char('k') => return Ok(Up),
-                Char('j') => return Ok(Down),
+                Char(' ') => return Ok(PageDownFull), // TODO not vim binding
+                Char('d') => match event.modifiers {
+                    KeyModifiers::CONTROL => return Ok(PageDownHalf),
+                    _ => continue,
+                },
+                Char('e') => match event.modifiers {
+                    KeyModifiers::CONTROL => return Ok(PageDownLine),
+                    _ => continue,
+                },
+                Char('f') => match event.modifiers {
+                    KeyModifiers::CONTROL => return Ok(PageDownFull),
+                    _ => continue,
+                },
                 Char('h') => return Ok(Left),
+                Char('j') => return Ok(Down),
+                Char('k') => return Ok(Up),
                 Char('l') => return Ok(Right),
-                Char(' ') => return Ok(PageDown),
+                Char('q') => return Ok(Quit),
                 _ => continue,
             },
             _ => continue,
@@ -58,16 +75,16 @@ fn await_input() -> Result<Action> {
 
 // display provided string
 // returns the byte index immediately after the last displayed grapheme
-fn display(
-    stdout: &mut Stdout,
-    s: &str,
-    pt: usize,
-    width: u16,
-    height: u16,
-) -> Result<DisplayInfo> {
+fn display(stdout: &mut Stdout, s: &str, pt: usize) -> Result<DisplayInfo> {
+    let (width, height) = terminal::size()?;
+    #[allow(non_snake_case)]
+    let DEBUG = false;
+    let usable_width = if DEBUG { width - 3 } else { width };
     let mut line = 0;
     let mut column = 0;
     let mut line_lengths = Vec::new();
+    let mut line_starts = Vec::new();
+    line_starts.push(pt);
 
     stdout
         .queue(cursor::SavePosition)?
@@ -84,8 +101,15 @@ fn display(
     // need to be careful about words longer than line
     for (i, g) in s[pt..].grapheme_indices(false) {
         // TODO handle double-width chars in monospace font
-        if column == width || is_newline(g) {
+        if column == usable_width || is_newline(g) {
             line_lengths.push(column);
+            if DEBUG {
+                stdout
+                    .queue(style::PrintStyledContent(
+                        format!("{}", line_starts[line as usize]).with(Color::White),
+                    ))?
+                    .queue(style::SetForegroundColor(Color::Red))?;
+            }
             column = 0;
             line += 1;
             if line == height {
@@ -93,9 +117,12 @@ fn display(
                     stdout,
                     DisplayInfo {
                         line_lengths: line_lengths,
+                        line_starts: line_starts,
                         next_char: pt + i,
                     },
                 );
+            } else {
+                line_starts.push(if is_newline(g) { pt + i + 1 } else { pt + i });
             }
             stdout.queue(cursor::MoveToNextLine(1))?;
         }
@@ -109,6 +136,7 @@ fn display(
         DisplayInfo {
             next_char: s.len(),
             line_lengths: line_lengths,
+            line_starts: line_starts,
         },
     );
 }
@@ -131,9 +159,8 @@ fn main() -> Result<()> {
         .queue(cursor::MoveTo(0, 0))?
         .flush()?;
     terminal::enable_raw_mode()?;
-    let (w, h) = terminal::size()?;
     let contents = fs::read_to_string(args.path)?;
-    let mut display_info = display(&mut stdout, &contents, 0, w, h)?;
+    let mut display_info = display(&mut stdout, &contents, 0)?;
     let mut target_column = 0;
     loop {
         use Action::*;
@@ -174,10 +201,19 @@ fn main() -> Result<()> {
                     stdout.execute(cursor::MoveLeft(1))?;
                 }
             }
-            PageDown => {
+            PageDownFull => {
                 if display_info.next_char < contents.len() {
-                    display_info = display(&mut stdout, &contents, display_info.next_char, w, h)?
+                    display_info = display(&mut stdout, &contents, display_info.next_char)?
                 }
+            }
+            PageDownLine => {
+                if display_info.line_starts.len() >= 2 {
+                    display_info = display(&mut stdout, &contents, display_info.line_starts[1])?
+                }
+            }
+            PageDownHalf => {
+                let half = display_info.line_starts.len() / 2;
+                display_info = display(&mut stdout, &contents, display_info.line_starts[half])?
             }
         };
     }
